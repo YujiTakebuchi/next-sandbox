0785291a2314a653eba66393cce12aa6b02c594e
YujiTakebuchi <takeyou.swift@gmail.com>
2023-01-13 01:52:47 +0900

    README追加と簡単な検証

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+# next-sandbox
+## 概要(Abstract)
+nextの機能や開発環境の設定とかを色々試すために作ったリポジトリ
+## 依存関係(Dependencies)
+next: `latest`
+<br>
+react: `18.2.0`
+<br>
+react-dom: `18.2.0`
+## 学習帳(Document)
+[https://rich-pressure-75c.notion.site/Next-js-90f1c82876f14fdc8eb954fcc44f7a58](https://rich-pressure-75c.notion.site/Next-js-90f1c82876f14fdc8eb954fcc44f7a58)
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 import styles from "../styles/Home.module.scss";
 
 export default function Home() {
@@ -23,34 +24,10 @@ export default function Home() {
             <h3>Sandbox &rarr;</h3>
             <p>ここで色々試してみよう</p>
           </a>
-
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <Link href="/posts/first-post/" className={styles.card}>
+            <h3>next/linkのナビゲーション &rarr;</h3>
+            <p>プロジェクト内ではプリフェッチしてくれたりするよ</p>
+          </Link>
         </div>
       </main>
 
