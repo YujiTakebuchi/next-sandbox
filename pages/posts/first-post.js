2ba1b32f45337e68acfc971a75729e4c189b1d13
YujiTakebuchi <takeyou.swift@gmail.com>
2023-01-14 23:05:55 +0900

    Image component and Head component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,24 @@
+import Head from "next/head";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function FirstPost() {
   return (
     <>
-      <h1>First Post</h1>
+      <Head>
+        <title>Next.jpのチュートリアルページだよ</title>
+        <Link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Next.js tutorial</h1>
+      <h2>First post</h2>
       <Link href={"/"}>TOPに戻る</Link>
+      <h2>static files</h2>
+      <Image
+        src="/images/sakura.jpg"
+        alt="かわいい"
+        width={1080}
+        height={810}
+      />
     </>
   );
 }
