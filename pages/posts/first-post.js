76691f6644eeaece0e55b2fd01f2bead4075382e
YujiTakebuchi <takeyou.swift@gmail.com>
2023-01-14 23:44:44 +0900

    Script component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,21 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import Script from "next/script";
 
 export default function FirstPost() {
   return (
     <>
       <Head>
         <title>Next.jpのチュートリアルページだよ</title>
-        <Link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
       <h1>Next.js tutorial</h1>
       <h2>First post</h2>
       <Link href={"/"}>TOPに戻る</Link>
